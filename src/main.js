f4857e84146e536336a61b1e673645a3c2399c47
Andy Prasetya <andyprasetya@aol.com>
2020-01-06 09:29:35 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 import '../node_modules/normalize.css/normalize.css';
 import './stylesheets/custom.css';
-import './stylesheets/font-awesome.min.css';
-// App's UI initialization
 console.log('loaded!');
